31f249541f0ab1faa1cce41a8758b0db71a56aa7
Nick Jewell <nick.jewell@gmail.com>
2012-05-12 21:23:56 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FamJam::Application.config.secret_token = 'a00ba3a5696e43b63a11d0c8bd31d944743b6dc25a23fefaf777b3fe682861db482ceebad10b20ea552111f0cfdfe56345c93a8e348ca8d9691f8e7a2bf65f22'
